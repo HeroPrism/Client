64fd7bbbf3295f1d4a70d56cb078547758af4811
Chris Caruth <chris.caruth@gmail.com>
2020-03-31 18:28:35 -0700

    Updates

---
+++ precache-manifest.c45707d336e6a1974687370594e21f25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a63070674b0744702f13161c62f1831",
+    "url": "/client/index.html"
+  },
+  {
+    "revision": "0ce36f456c3c5604c18c",
+    "url": "/client/static/css/main.7d51eeda.chunk.css"
+  },
+  {
+    "revision": "5681d151f7a8d8af51fb",
+    "url": "/client/static/js/2.dcea9a8f.chunk.js"
+  },
+  {
+    "revision": "8934ea3881e73965e39ae115021a9f54",
+    "url": "/client/static/js/2.dcea9a8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ce36f456c3c5604c18c",
+    "url": "/client/static/js/main.629c490f.chunk.js"
+  },
+  {
+    "revision": "cc640e19c81fbf30f704",
+    "url": "/client/static/js/runtime-main.2e2c2fb4.js"
+  },
+  {
+    "revision": "e4f26ce899df4b376147c395d3428c2e",
+    "url": "/client/static/media/LoginHero.e4f26ce8.png"
+  }
+]);
