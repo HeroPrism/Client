9dcdcd82711e3faca149912d9fd24f93e104fd56
Chris Caruth <chris.caruth@gmail.com>
2020-04-02 22:41:23 -0700

    improved layout for task creator

--- src/components/Tasks/TaskCreator.tsx
+++ src/components/Tasks/TaskCreator.tsx
@@ -1,5 +1,5 @@
-import React, { useState, FC, useEffect, useRef, RefObject, useCallback, FormEvent, ChangeEvent } from 'react';
-import { Box, TextInput, Button, Text, TextArea } from 'grommet';
+import React, { useState, FC, useEffect, useRef, RefObject, useCallback, FormEvent, ChangeEvent, useContext } from 'react';
+import { Box, TextInput, Button, Text, TextArea, Heading, ResponsiveContext } from 'grommet';
 import styles from '../../styles.module.scss';
 import { Validator, FailureReason } from '../../lib/validation';
 import { field, required } from '../../lib/validation/rules';
@@ -10,6 +10,7 @@ import { RouteName, path } from '../../routing';
 
 export const TaskCreator: FC = (props) => {
     const history = useHistory();
+    const size = useContext(ResponsiveContext);
     const validator = new Validator({
         title: field(required()),
         description: field(required()),
@@ -67,48 +68,68 @@ export const TaskCreator: FC = (props) => {
     );
 
     return (
-        <Box pad="medium" animation={["fadeIn", "slideUp"]}>
+        <Box pad={"medium"} animation={["fadeIn", "slideUp"]}>
             {!taskCreated &&
-                <Box
-                    as="form"
-                    fill="vertical"
-                    overflow="auto"
-                    margin={{ top: "medium" }}
-                    onSubmit={onCreateTask}
-                >
-                    <Box margin={{ bottom: "small" }}>
-                        <Text>Zipcode</Text>
-                        <TextInput
-                            onChange={onZipCodeChange}
-                        ></TextInput>
-                        {errors.zipCode && <Text color="red">{errors.zipCode}</Text>}
+                <div>  
+                    <Box background="neutral" pad="medium" border={{ side: "all", color: "red"}} round="xsmall">
+                        <Text size="small">Slow the Spread of Coronavirus (COVID-19): Please practice social distancing while requesting help from our heroes. Please visit <a href="https://www.cdc.gov" target="_blank">www.cdc.gov</a> for recommended guidlines.</Text>
                     </Box>
-                    <Box margin={{ bottom: "small" }}>
-                        <Text>Title</Text>
-                        <TextInput
-                            onChange={onTitleChange}
-                        ></TextInput>
-                        {errors.title && <Text color="red">{errors.title}</Text>}
+                    <Box margin={{ top: size == "small" ? "large" : "medium", bottom: size == "small" ? "large" : "none"  }}>
+                        <Text size="large" color="primary">Create a request for help</Text>
                     </Box>
-                    <Box margin={{ bottom: "small" }}>
-                        <Text>Description</Text>
-                        <TextArea
-                            onChange={onDescriptionChange}
-                            style={{ minHeight: "300px" }}
-                            resize={false}
-                        />
-                        {errors.description && <Text color="red">{errors.description}</Text>}
+                    <Box
+                        as="form"
+                        fill="vertical"
+                        overflow="auto"
+                        margin={{ top: "medium" }}
+                        onSubmit={onCreateTask}
+                    >
+                        <Box margin={{ bottom: size == "small" ? "large" : "medium" }}>
+                            <Box margin={{ bottom: "small" }}>
+                                <Text>Zipcode</Text>
+                            </Box>
+                            <TextInput
+                                onChange={onZipCodeChange}
+                            ></TextInput>
+                            {errors.zipCode && <Text color="red">{errors.zipCode}</Text>}
+                            <Box margin={{ top: "xsmall" }}>
+                                <Text color="#333333" size="small">We'll use an approximation to create a pin on the map.</Text>
+                            </Box>
+                        </Box>
+                        <Box margin={{ bottom: size == "small" ? "large" : "medium" }}>
+                            <Box margin={{ bottom: "small" }}>
+                                <Text>Title</Text>
+                            </Box>
+                            <TextInput
+                                onChange={onTitleChange}
+                            ></TextInput>
+                            {errors.title && <Text color="red">{errors.title}</Text>}
+                            <Box margin={{ top: "xsmall" }}>
+                                <Text color="#333333" size="small">Keep it short and to the point.</Text>
+                            </Box>
+                        </Box>
+                        <Box margin={{ bottom: size == "small" ? "large" : "medium" }}>
+                            <Box margin={{ bottom: "small" }}>
+                                <Text>Description</Text>
+                            </Box>
+                            <TextArea
+                                onChange={onDescriptionChange}
+                                style={{ minHeight: "300px" }}
+                                resize={false}
+                            />
+                            {errors.description && <Text color="red">{errors.description}</Text>}
+                        </Box>
+                        <Box flex={false} as="footer" align="end" margin={{ vertical: "medium" }}>
+                            <Button primary
+                                color="primary"
+                                type="submit"
+                                className={styles.btn}
+                            >
+                                Create
+                            </Button>
+                        </Box>
                     </Box>
-                    <Box flex={false} as="footer" align="center" margin={{ vertical: "medium" }}>
-                        <Button primary
-                            color="primary"
-                            type="submit"
-                            className={styles.btn}
-                        >
-                            Create
-                        </Button>
-                    </Box>
-                </Box>
+                </div>
             }
             {taskCreated &&
                 <Box>
